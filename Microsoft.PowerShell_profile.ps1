478bb12303b129fa4fdd2076c253083946319de4
Ilya Samokhin <Ilya.Samokhin.97@yandex.ru>
2025-01-03 16:55:37 +0300

    init
    display current datetime in shell prompt
    make 'y' an alias for yazi to cd to cwd after exiting

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,16 @@
+function Prompt {
+    $gray = "`e[90m"
+    $reset = "`e[0m"
+
+    "$gray$((Get-Date).ToString("yy/MM/dd HH:mm:ss"))$reset PS $(Get-Location)>"
+}
+
+function y {
+    $tmp = [System.IO.Path]::GetTempFileName()
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath $cwd
+    }
+    Remove-Item -Path $tmp
+}
